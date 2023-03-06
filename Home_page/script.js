1ba30442f9f78fa575266e9e70c5fa9d2396bc92
davietop <oyatoyedavid@gmail.com>
2023-03-06 15:56:19 +0100

    First commit

---
+++ Home_page/script.js
@@ -0,0 +1,62 @@
+const btnRight = document.querySelector(".right");
+const btnLeft = document.querySelector(".left");
+const dots = document.querySelectorAll(".dot");
+const move = document.querySelector(".move");
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+window.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") plusSlides(1);
+  if (e.key === "ArrowLeft") plusSlides(-1);
+});
+
+let slideIndex2 = 0;
+showSlides2();
+
+function showSlides2() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex2++;
+  if (slideIndex2 > slides.length) {
+    slideIndex2 = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2 - 1].style.display = "block";
+  dots[slideIndex2 - 1].className += " active";
+  setTimeout(showSlides2, 20000);
+}
